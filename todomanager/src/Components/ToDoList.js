522002f77f79efe13db8e9445d2526e67c34e4be
Shanmukh <shanmukh.rao15@gmail.com>
2018-01-28 14:28:58 +0000

    Update to learning.

--- todomanager/src/Components/ToDoList.js
+++ todomanager/src/Components/ToDoList.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
-import { Card, Button, CardTitle, CardText } from 'reactstrap';
+import { Card, Button, CardTitle, CardText,ListGroup } from 'reactstrap';
+import ToDo from './ToDo';
 
 export default class ToDoList extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {todos : [{id : 1, done : true, text : "learn redux-react"},{id : 1, done : false, text : "learn reactstrap"},
+                                {id : 3, done : false, text : "learn react"}]};
+    }
+    
     render() {
+        const todoset = this.state.todos.map((item) => <ToDo item={item} />);
+        
         return (
         <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-            
+            <ListGroup>
+                {todoset}
+            </ListGroup>
         </Card>
     );
 
