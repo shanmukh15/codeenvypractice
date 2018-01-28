c7bcc80b84aa9423a75e8fde611115e90c2b4284
Shanmukh <shanmukh.rao15@gmail.com>
2018-01-28 14:06:13 +0000

    Intermediate check with Todo body editor

---
+++ todomanager/src/Components/ToDoList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { Card, Button, CardTitle, CardText } from 'reactstrap';
+
+export default class ToDoList extends Component {
+    render() {
+        return (
+        <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+            
+        </Card>
+    );
+
+    }
+}
+
